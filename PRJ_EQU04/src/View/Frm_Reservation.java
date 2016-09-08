8198752b1609a2cf8410c81417f42bd8347b5c58
Vivien Dumont <dumontvivien54@gamil.com>
2016-09-08 13:57:15 -0400

    frm_reser hérite base

--- PRJ_EQU04/src/View/Frm_Reservation.java
+++ PRJ_EQU04/src/View/Frm_Reservation.java
@@ -31,12 +31,13 @@ public class Frm_Reservation extends Frm_Base {
 	 * Create the frame.
 	 */
 	public Frm_Reservation() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		super();
+		/*setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane.setLayout(null);*/
 	}
 
 }
