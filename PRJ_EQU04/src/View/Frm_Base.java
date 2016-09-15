1cd514d0e639f7e3f80b059dd26cbc83d1ca0c45
Vivien54 <dumontvivien54@gmail.com>
2016-09-15 14:30:32 -0400

    Revert "reser"
    This reverts commit 3b2e5bb6060c687ffc397195137ed479a5dd3afb.

--- PRJ_EQU04/src/View/Frm_Base.java
+++ PRJ_EQU04/src/View/Frm_Base.java
@@ -11,16 +11,12 @@ import javax.swing.border.EmptyBorder;
 
 public class Frm_Base extends JFrame {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	protected JButton btnNaviguerGauche;
-	protected JButton btnAjouter;
-	protected JButton btnSupprimer;
-	protected JButton btnConsulter;
-	protected JButton btnNaviguer;
-	protected JButton btnFin;
-	protected JButton btnNaviguer_1;
-	protected JButton btnModifier;
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -48,7 +44,7 @@ public class Frm_Base extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		btnNaviguerGauche = new JButton("Debut");
+		JButton btnNaviguerGauche = new JButton("Debut");
 		btnNaviguerGauche.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -56,19 +52,19 @@ public class Frm_Base extends JFrame {
 		btnNaviguerGauche.setBounds(36, 667, 117, 29);
 		contentPane.add(btnNaviguerGauche);
 		
-		btnAjouter = new JButton("Ajouter");
+		JButton btnAjouter = new JButton("Ajouter");
 		btnAjouter.setBounds(294, 667, 117, 29);
 		contentPane.add(btnAjouter);
 		
-		btnSupprimer = new JButton("Supprimer");
+		JButton btnSupprimer = new JButton("Supprimer");
 		btnSupprimer.setBounds(423, 667, 117, 29);
 		contentPane.add(btnSupprimer);
 		
-		btnConsulter = new JButton("Consulter");
+		JButton btnConsulter = new JButton("Consulter");
 		btnConsulter.setBounds(682, 667, 117, 29);
 		contentPane.add(btnConsulter);
 		
-		btnNaviguer = new JButton("Avant");
+		JButton btnNaviguer = new JButton("Avant");
 		btnNaviguer.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -76,15 +72,15 @@ public class Frm_Base extends JFrame {
 		btnNaviguer.setBounds(165, 667, 117, 29);
 		contentPane.add(btnNaviguer);
 		
-		btnFin = new JButton("Fin");
+		JButton btnFin = new JButton("Fin");
 		btnFin.setBounds(940, 667, 117, 29);
 		contentPane.add(btnFin);
 		
-		btnNaviguer_1 = new JButton("Apres");
+		JButton btnNaviguer_1 = new JButton("Apres");
 		btnNaviguer_1.setBounds(811, 667, 117, 29);
 		contentPane.add(btnNaviguer_1);
 		
-		btnModifier = new JButton("Modifier");
+		JButton btnModifier = new JButton("Modifier");
 		btnModifier.setBounds(553, 667, 117, 29);
 		contentPane.add(btnModifier);
 	}
