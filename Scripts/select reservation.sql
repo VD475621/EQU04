7da66c25b7e019620f6fa65b7dca2397a44ae24a
Vivien Dumont <dumontvivien54@gamil.com>
2016-12-08 10:54:23 -0500

    save select rapport

--- Scripts/select reservation.sql
+++ Scripts/select reservation.sql
@@ -7,3 +7,12 @@ select * from reservation;
 select * from arrive;
 
 select * from depart;
+
+select r.Idreser, r.DATERESER, r.DATEDEBUT, r.DATEFIN, nc.FKNOCHAM, ch.FKCODTYPCHA, nc.FKIDCLI, nc.nom
+from reservation r, chambre ch, (select d.FKIDRESER, d.FKNOCHAM, nb.nom, nb.FKIDCLI 
+                                from  de d
+                                full outer join ( select a.FKNOCHAM, a.FKIDCLI, a.FKIDRESER, c.nom  from ARRIVE a, client c
+                                                  where a.FKIDCLI=c.IDCLI order by a.FKIDRESER
+                                                ) nb
+                                on d.FKIDRESER=nb.FKIDRESER and d.FKNOCHAM=nb.FKNOCHAM) nc
+where r.IDRESER=nc.FKIDRESER and ch.NOCHAM=nc.FKNOCHAM order by r.idreser;
